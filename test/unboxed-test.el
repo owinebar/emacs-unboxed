495ee5db4686ba2f67c758daed8e3f12b2e7aa1f
Onnie Lynn Winebarger <owinebar@gmail.com>
2023-04-08 13:41:51 -0400

    Placeholder for testing

---
+++ test/unboxed-test.el
@@ -0,0 +1,33 @@
+;;; unboxed-test.el --- Test unboxed routines        -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023  Onnie Lynn Winebarger
+
+;; Author: Onnie Lynn Winebarger
+;; Keywords: 
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; These functions are for testing unboxed package manager
+
+;;; Code:
+
+
+
+
+
+(provide 'unboxed-test)
+;;; unboxed-test.el ends here
+
